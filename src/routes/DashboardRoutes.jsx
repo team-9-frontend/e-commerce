c3913fcef17c0c6389582eddb303a55289a85a77
ziad-wdev <ziadahmed2371@gmail.com>
2026-07-04 22:33:51 +0300

    Refactor imports to use relative paths

--- src/routes/DashboardRoutes.jsx
+++ src/routes/DashboardRoutes.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
-import DashboardLayout from 'src/layouts/DashboardLayout'
-import AdminCart from 'src/pages/dashboard/Cart'
-import AdminDashboard from 'src/pages/dashboard/Dashboard'
-import AdminLogin from 'src/pages/dashboard/Login'
-import AdminUsers from 'src/pages/dashboard/Users'
-import AdminDynamicOrder from 'src/pages/dashboard/orders/DynamicOrders'
-import AdminOrders from 'src/pages/dashboard/orders/Orders'
-import AdminProducts from 'src/pages/dashboard/products/Products'
-import AdminProductCreate from 'src/pages/dashboard/products/ProductsCreate'
-import AdminProductEdit from 'src/pages/dashboard/products/ProductsEdit'
+
+import DashboardLayout from '../layouts/DashboardLayout'
+import AdminCart from '../pages/dashboard/Cart'
+import AdminDashboard from '../pages/dashboard/Dashboard'
+import AdminLogin from '../pages/dashboard/Login'
+import AdminUsers from '../pages/dashboard/Users'
+import AdminDynamicOrder from '../pages/dashboard/orders/DynamicOrders'
+import AdminOrders from '../pages/dashboard/orders/Orders'
+import AdminProducts from '../pages/dashboard/products/Products'
+import AdminProductCreate from '../pages/dashboard/products/ProductsCreate'
+import AdminProductEdit from '../pages/dashboard/products/ProductsEdit'
 
 export default function DashboardRoutes() {
   return (
