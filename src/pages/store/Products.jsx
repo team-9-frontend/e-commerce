c995914ae44a505a537e6de64014bb8c1f45deb7
AhmedHamdy434 <cmpunkthebest11@gmail.com>
2026-07-14 20:56:38 +0300

    design products page with search and filter features

--- src/pages/store/Products.jsx
+++ src/pages/store/Products.jsx
@@ -1,3 +1,20 @@
+import InputSearch from '../../components/store/products/InputSearch'
+import FilterSection from '../../components/store/products/FilterSection'
+import StoreProductCards from '../../components/store/products/StoreProductCards'
+import ActiveFilters from '../../components/store/products/ActiveFilters'
+
 export default function Products() {
-  return <h1>Products Page</h1>
+  return (
+    <section className="space-y-6 py-6 md:py-10">
+      <div className="w-full space-y-4">
+        <InputSearch />
+        <ActiveFilters />
+      </div>
+
+      <div className="flex flex-col items-start gap-6 md:flex-row">
+        <FilterSection />
+        <StoreProductCards />
+      </div>
+    </section>
+  )
 }
