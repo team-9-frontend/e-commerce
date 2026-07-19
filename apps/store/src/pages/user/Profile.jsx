2edf25fa15f731467b8eab6f2faef61398fa2ced
azzaahmed10 <aaza5690@gmail.com>
2026-07-20 02:54:30 +0300

    Complete Task 5 - Profile page view user data

--- apps/store/src/pages/user/Profile.jsx
+++ apps/store/src/pages/user/Profile.jsx
@@ -1,3 +1,20 @@
+
+import { useCurrentUser } from '@repo/api'
+
 export default function Profile() {
-  return <h1>Profile Page</h1>
+  const { data, isLoading, error } = useCurrentUser()
+
+  if (isLoading) return <h2>Loading...</h2>
+
+  if (error) return <h2>Error loading profile</h2>
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+
+      <p>Name: {data?.username}</p>
+      <p>Email: {data?.email}</p>
+      <p>Role: {data?.role}</p>
+    </div>
+  )
 }
